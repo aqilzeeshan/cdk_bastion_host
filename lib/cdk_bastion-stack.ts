04366eb3cc1b7f5d19947c52c73e11793730bed9
aqilzeeshan <aqilzeeshan@users.noreply.github.com>
2021-03-22 23:29:57 +0000

    Add autoscaling

--- lib/cdk_bastion-stack.ts
+++ lib/cdk_bastion-stack.ts
@@ -3,6 +3,8 @@ import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as s3 from '@aws-cdk/aws-s3';
 import { hostname } from 'os';
+import autoscaling = require('@aws-cdk/aws-autoscaling');
+import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
 
 export class CdkBastionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -33,6 +35,8 @@ export class CdkBastionStack extends cdk.Stack {
     vpc.addFlowLog('FlowLogCloudWatch', {
       trafficType: ec2.FlowLogTrafficType.REJECT
     });
+
+
 
     /**
      * A bastion host functions as an instance used to access servers and resources in a VPC without open up the complete VPC on a network level. You can use bastion hosts using a standard SSH connection targeting port 22 on the host. As an alternative, you can connect the SSH connection feature of AWS Systems Manager Session Manager, which does not need an opened security group.
@@ -69,9 +73,18 @@ export class CdkBastionStack extends cdk.Stack {
         allowAllOutbound:true
       })
       //Allow ssh from VPC
-      webserverSecurityGroup.addIngressRule(ec2.Peer.ipv4('10.0.0.0/16'),ec2.Port.tcp(22),"Allow ssh access from the VPC")
+      //webserverSecurityGroup.addIngressRule(
+      //  ec2.Peer.ipv4('10.0.0.0/16'),
+      //  ec2.Port.tcp(22),
+      //  "Allow ssh access from the VPC"
+      //)
+      
       //Allow ssh from anywhere in the world
-      //mySecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'allow ssh access from the world');
+      webserverSecurityGroup.addIngressRule(
+        ec2.Peer.anyIpv4(),
+        ec2.Port.tcp(22), 
+        'allow ssh access from the world'
+      );
       
       //allow requests from any IP to port 80
       webserverSecurityGroup.addIngressRule(
@@ -80,20 +93,92 @@ export class CdkBastionStack extends cdk.Stack {
         'allow ingress http traffic'                                                                                                                                                     
       )
 
-
       //as per https://github.com/aws/aws-cdk/issues/12848
       const subnetSelection : ec2.SubnetSelection = {
         subnetType:ec2.SubnetType.PUBLIC
       };
 
       //setup web instance in public subnet
-      const webServer = new ec2.Instance(this, "WebInstance",{
+      /**
+       * 
+       *const webServer = new ec2.Instance(this, "WebInstance",{
         instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
         machineImage: new ec2.AmazonLinuxImage(),
         vpc: vpc,
         securityGroup:webserverSecurityGroup,
         vpcSubnets:subnetSelection
       })
+       */
+
+      /*
+      //To create Ubuntu webserver with Apache running
+
+      const userData = ec2.UserData.forLinux();
+      userData.addCommands('apt-get update -y','update apt-get')
+      userData.addCommands('apt-get upgrade -y','upgrade apt-get')
+      userData.addCommands('apt-get install apache2 stress -y','install apache2 and stress tool')
+      userData.addCommands('systemctl start apache2','start apache2')
+      userData.addCommands('systemctl enable --now apache2','enable apache2')
+
+      //Ubuntu server found through lookup
+      const linux = ec2.MachineImage.genericLinux({
+        'us-east-1': 'ami-056db1277deef2218',
+      });
+
+      const webServer = new ec2.Instance(this, "WebInstance",{
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
+        machineImage: linux,
+        vpc: vpc,
+        securityGroup:webserverSecurityGroup,
+        vpcSubnets:subnetSelection,
+        userData:userData,
+        keyName:'JenkinsCF'
+      })
+      */
+
+      //AMI created based on web server above
+      const linux = ec2.MachineImage.genericLinux({
+        'us-east-1': 'ami-075014f01b8b5fd16',
+      });
+
+      const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+        machineImage: linux,
+        vpc:vpc,
+        securityGroup:webserverSecurityGroup,
+        keyName:'JenkinsCF',
+        minCapacity: 1,
+        maxCapacity: 3,
+        associatePublicIpAddress:true,
+        vpcSubnets:subnetSelection,
+      });
+
+      const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+        vpc,
+        internetFacing: true
+      });
+  
+      const listener = lb.addListener('Listener', {
+        port: 80,
+      });
+  
+      listener.addTargets('Target', {
+        port: 80,
+        targets: [asg]
+      });
+  
+      listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+  
+      asg.scaleOnCpuUtilization('KeepSpareCPU', {
+        targetUtilizationPercent: 50
+      });
+      
+      new cdk.CfnOutput(this, 'Loadbalancer DNS', { value: lb.loadBalancerDnsName });
+  
+
+
+
+
 
       /**
        *       
@@ -105,6 +190,8 @@ export class CdkBastionStack extends cdk.Stack {
       https://mechanicalrock.github.io/2020/07/20/private-bastions.html
       https://docs.aws.amazon.com/cdk/api/latest/docs/aws-backup-readme.html
        */
+
+      
 
   }
 }
